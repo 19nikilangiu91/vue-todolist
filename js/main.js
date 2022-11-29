0612c34f487c153d130caf41ba0cfcf271f08ee6
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-29 16:47:51 +0100

    Upgrade checkTask js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,19 @@ const { createApp } = Vue
             }, 
             { 
                 text: 'Fare il bucato',
-                 done: false 
+                 done: false
+            },
+            { 
+                text: 'Fare i piatti', 
+                done: false 
+            }, 
+            { 
+                text: 'Fare la benzina',
+                done: true 
+            }, 
+            { 
+                text: 'Fare il letto',
+                 done: false
             }
         ],
       }
@@ -28,18 +40,23 @@ const { createApp } = Vue
     methods: {
         addTask(){
             if(this.newTask.length< 5 || this.newTask === ''){
-                // Inseriamo l'errore
+                // Inseriamo l'errore se i caratteri sono minori di 5 o il newTask è vuoto.
                 this.errore = true;
             }else{
-                this.tasks.push({text: this.newTask});
-                // Togliamo l'errore
+                this.tasks.push({text: this.newTask,done: false});
+                // Togliamo l'errore se i caratteri sono maggiori di 5.
                 this.errore = false;
             }
 
             this.newTask = '';
         },
+
         deleteTask(indice){
             this.tasks.splice(indice, 1);
+        },
+
+        checkTask(indice){
+            this.task[indice].done = !this.task[indice].done;
         }
     }
   }).mount("#myApp")
